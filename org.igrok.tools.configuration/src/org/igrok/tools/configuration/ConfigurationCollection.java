425a4a580da18902c3eac1733fa8b32e253be058
oleg <oleg@oleg-pc>
2018-03-17 21:46:43 +0100

    added json reading to configuration

--- org.igrok.tools.configuration/src/org/igrok/tools/configuration/ConfigurationCollection.java
+++ org.igrok.tools.configuration/src/org/igrok/tools/configuration/ConfigurationCollection.java
@@ -4,10 +4,15 @@
 package org.igrok.tools.configuration;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Dictionary;
-import java.util.Hashtable;
+import java.util.List;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  * @author oleg
@@ -15,19 +20,37 @@ import java.util.Hashtable;
  */
 public class ConfigurationCollection {
 
-	private Dictionary<String, Object> configuration;
-	
+	private JSONObject configuration;
+
 	/**
 	 * 
 	 */
 	public ConfigurationCollection() {
-		this.configuration = new Hashtable<String, Object>();
 	}
-	
-	public void loadFromFile(String path) throws FileNotFoundException {
-		if(Paths.get(path) == null) {
+
+	/**
+	 * Loads and replaces configuration values from json file
+	 * 
+	 * @param path
+	 * @throws IOException
+	 * @throws ParseException
+	 */
+	public void loadFromFile(String path) throws IOException, ParseException {
+		Path filePath = Paths.get(path);
+		if (filePath == null) {
 			throw new FileNotFoundException("specified file does not exists");
 		}
+		if (!Files.isReadable(filePath)) {
+			throw new IOException("file is not readeable");
+		}
+		List<String> data = Files.readAllLines(filePath);
+		String jsonString = "";
+		for (String dataLine : data) {
+			jsonString += dataLine + "\r\n";
+		}
+		JSONParser parser = new JSONParser();
+		Object dataObject = parser.parse(jsonString);
+		this.configuration = (JSONObject) dataObject;
 	}
 
 }
